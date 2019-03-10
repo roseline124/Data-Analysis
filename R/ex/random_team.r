ed8734a8ba15172f23ceb2ef5ef937d3d4f49959
roseline <guseod24@gmail.com>
2019-03-11 01:42:11 +0900

    analyzed criminal data

--- R/ex/random_team.r
+++ R/ex/random_team.r
@@ -1,5 +1,4 @@
-read.table(file="dataset/DA.csv", header=TRUE, fill=TRUE)
+nba <- read.csv(file="dataset/nba2013.csv", header=TRUE, fill=TRUE, sep=",")
+
+students <- read.table(file="C:/Users/guseo/data_analysis/R/dataset/DA.xlsx", header=TRUE, fileEncoding='utf-8', fill=TRUE )
 students
-
-
-nba <- read.csv(file="dataset/nba2013.csv", header=TRUE, fill=TRUE)
